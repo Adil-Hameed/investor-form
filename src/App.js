892be050a21319954688735737e2a26d8dbd3f2a
engradilhameed <adilhameed200@gmail.com>
2023-05-28 17:30:49 +0500

    success page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { Route, Routes, useLocation } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,7 +6,7 @@ import { useEffect } from 'react';
 import Step2 from './pages/Step2';
 import Step3 from './pages/Step3';
 import Step4 from './pages/Step4';
-
+import Success from './pages/Success';
 
 function App() {
   const { pathname } = useLocation();
@@ -21,6 +20,7 @@ function App() {
       <Route path="/step2" element={<Step2 />} />
       <Route path="/step3" element={<Step3 />} />
       <Route path="/step4" element={<Step4 />} />
+      <Route path="/success" element={<Success />} />
     </Routes>
   )
 }
